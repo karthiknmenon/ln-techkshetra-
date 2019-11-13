7018a292224c59c1b52066c6e6e7d473be5f595a
mojojojo20 <menonk484@gmail.com>
2019-11-13 19:46:16 +0530

    favicon

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,6 +5,7 @@ exports.components = {
   "component---node-modules-narative-gatsby-theme-novela-src-templates-articles-template-tsx": () => import("../node_modules/@narative/gatsby-theme-novela/src/templates/articles.template.tsx" /* webpackChunkName: "component---node-modules-narative-gatsby-theme-novela-src-templates-articles-template-tsx" */),
   "component---node-modules-narative-gatsby-theme-novela-src-templates-article-template-tsx": () => import("../node_modules/@narative/gatsby-theme-novela/src/templates/article.template.tsx" /* webpackChunkName: "component---node-modules-narative-gatsby-theme-novela-src-templates-article-template-tsx" */),
   "component---node-modules-narative-gatsby-theme-novela-src-templates-author-template-tsx": () => import("../node_modules/@narative/gatsby-theme-novela/src/templates/author.template.tsx" /* webpackChunkName: "component---node-modules-narative-gatsby-theme-novela-src-templates-author-template-tsx" */),
+  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---node-modules-narative-gatsby-theme-novela-src-templates-articles-template-tsx": hot(preferDefault(require("/Users/karthik/Desktop/ln-techkshetra-/node_modules/@narative/gatsby-theme-novela/src/templates/articles.template.tsx"))),
   "component---node-modules-narative-gatsby-theme-novela-src-templates-article-template-tsx": hot(preferDefault(require("/Users/karthik/Desktop/ln-techkshetra-/node_modules/@narative/gatsby-theme-novela/src/templates/article.template.tsx"))),
   "component---node-modules-narative-gatsby-theme-novela-src-templates-author-template-tsx": hot(preferDefault(require("/Users/karthik/Desktop/ln-techkshetra-/node_modules/@narative/gatsby-theme-novela/src/templates/author.template.tsx"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/karthik/Desktop/ln-techkshetra-/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/karthik/Desktop/ln-techkshetra-/src/pages/404.js")))
 }
 
