ffa030258194fd119afc364fc9017d9af0aec902
mojojojo20 <menonk484@gmail.com>
2020-05-23 11:11:30 +0530

    :recycle: remove node_modules

--- node_modules/@narative/gatsby-theme-novela/gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-exports.onInitialClientRender = require('./src/gatsby/browser/onInitialClientRender');
-exports.onRouteUpdate = require('./src/gatsby/browser/onRouteUpdate');
-exports.shouldUpdateScroll = require('./src/gatsby/browser/shouldUpdateScroll');
